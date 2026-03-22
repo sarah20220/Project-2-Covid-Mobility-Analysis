78268f5c7d59196b40d041727b529811c9ae0330
Sarah <sarah.matzat20@gmail.com>
2026-03-22 02:42:42 -0400

    Added More

--- kd_trees.py
+++ kd_trees.py
@@ -1,14 +1,71 @@
-def dimensionCheck(k):
-    if 6 >= k >= 1:
-        return k
+import numpy as np
+import sys
 
 
-class Node:
-    def _init_(self,point,k,left=None,right=None):
+
+class KDTree(object):
+    def __init__(self,point,k,left=None,right=None):
         self.point = point
         self.left = left
         self.right = right
-        self.k = dimensionCheck(k)
+        self.k = self.dimensionCheck(k)
+
+    @staticmethod
+    def dimensionCheck(k):
+        if 6 >= k >= 1:
+            return k
+
+    @staticmethod
+    def findMedian(dataPoints,k):
+        return np.median(dataPoints,k)
+
+    @staticmethod
+    def getDistance(target,point):
+        result = 0
+        for i in range(len(target)):
+            result += (target[i] - point[i])**2
+        return np.sqrt(result)
+    
+# Operations: Insert, InsertMany, Delete, FindNearestK, FindNode, Construction?
+
+    @staticmethod
+    def buildTree(dataPoints, depth):
+        if not dataPoints:
+            return None
+        
+        k = len(dataPoints[0])
+        axis = depth % k
+        sortedData = sorted(dataPoints, key=lambda p: p[axis])
+        #print(sortedData)
+        medianIndex = len(sortedData) // 2
+        medianData = sortedData[medianIndex]
+        #print("Dimension:",axis,medianData)
+
+        node = KDTree(medianData, axis)
+        node.left = KDTree.buildTree(sortedData[:medianIndex], depth + 1)
+        node.right = KDTree.buildTree(sortedData[medianIndex + 1:], depth + 1)
+
+        return node
+
+    @staticmethod
+    def kNearestNeighbors(target,dataPoints,k):
+        distances = []
+
+        for point in dataPoints:
+            distances.append((getDistance(target,point),point))
+        sortedDistances = sorted(distances)
+
+        if target in dataPoints:
+            return sortedDistances[:k]
+        
+        return sortedDistances[1:k+1]
 
 
-# Operations: Insert, Delete, Find, Construction?
+def main():
+    data = ([1,2,3,4,5,6],[2,3,4,5,6,7],[-1,-2,-3,-4,-5,-6],[-2,-3,-4,-5,-6,-7],[3,4,5,6,7,8])
+    myKDTree = KDTree.buildTree(data,1)
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
